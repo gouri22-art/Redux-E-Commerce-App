69e9c1d1bdf4bd89a9eefb0e7a9df39f1b97659a
gouri22-art <prajaktapk2208@gmail.com>
2025-02-22 16:50:41 +0530

    Routes added to the App

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-    <App />
-    </ChakraProvider>
+  <Provider store={store}>
+  <App />
+
+  </Provider>
+    
     
   </StrictMode>,
 )
